63c08b5668f7ead3e07a531d2927a70cddce0588
deveshaha <devesh_hanumante@outlook.com>
2023-01-27 12:44:37 +0100

    update: implement methods to load data into firebase

--- app/src/main/java/com/dam/pruebafirebaseauth/MainActivity.java
+++ app/src/main/java/com/dam/pruebafirebaseauth/MainActivity.java
@@ -8,10 +8,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.dam.pruebafirebaseauth.model.Coche;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -20,7 +24,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private FirebaseDatabase fbd;
     private DatabaseReference dbref;
     TextView tv_user;
-    Button btn_logout, btnSetValue, btnPush;
+    Button btn_logout, btnSetValue, btnPush, btnUpdateChildren, btnSetValueObj,
+            btnUpdateChildrenObj, btnModifAtr, btnRemove, btnSetValueNull;
     int cont;
 
     @Override
@@ -32,6 +37,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btn_logout = findViewById(R.id.btn_logout);
         btnSetValue = findViewById(R.id.btnSetValue);
         btnPush = findViewById(R.id.btnPush);
+        btnUpdateChildren = findViewById(R.id.btnUpdateChildern);
+        btnSetValueObj = findViewById(R.id.btnSetValueObj);
+        btnUpdateChildrenObj = findViewById(R.id.btnUpdateChildrenObj);
+        btnModifAtr = findViewById(R.id.btnModifAtr);
+        btnRemove = findViewById(R.id.btnRemove);
+        btnSetValueNull = findViewById(R.id.btnSetValueNull);
 
         fbd = FirebaseDatabase.getInstance();
         dbref = fbd.getReference();
@@ -45,6 +56,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btn_logout.setOnClickListener(this);
         btnSetValue.setOnClickListener(this);
         btnPush.setOnClickListener(this);
+        btnUpdateChildren.setOnClickListener(this);
+        btnSetValueObj.setOnClickListener(this);
+        btnUpdateChildrenObj.setOnClickListener(this);
+        btnModifAtr.setOnClickListener(this);
+        btnRemove.setOnClickListener(this);
+        btnSetValueNull.setOnClickListener(this);
     }
 
     private void logout() {
@@ -65,7 +82,57 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             probarSetValue();
         } else if (view.getId() == R.id.btnPush){
             probarPush();
+        } else if (view.getId() == R.id.btnUpdateChildern){
+            probarUpdateChildren();
+        } else if (view.getId() == R.id.btnSetValueObj){
+            probarSetValueObj();
+        } else if (view.getId() == R.id.btnUpdateChildrenObj){
+            probarUpdateChildrenObj();
+        } else if (view.getId() == R.id.btnModifAtr){
+            probarModifAtr();
+        } else if (view.getId() == R.id.btnRemove){
+            probarRemove();
+        } else if (view.getId() == R.id.btnSetValueNull){
+            probarSetValueNull();
         }
+    }
+
+    private void probarSetValueNull() {
+        dbref.child("Animales/animal3").setValue(null);
+    }
+
+    private void probarRemove() {
+        // Se puede poner asi y te ahorras poner más childs
+        dbref.child("Coches/coche11").removeValue();
+    }
+
+    private void probarModifAtr() {
+        //Se puede poner asi o
+        dbref.child("Coches").child("coche11/modelo").setValue("LEON");
+    }
+
+    private void probarUpdateChildrenObj() {
+        Map<String, Object> estructuraCoches = new HashMap<String, Object>();
+        estructuraCoches.put("coche" + (cont + (cont++ + 10)), new Coche("2345CDF", "MERCEDES BENZ", "CLASE A"));
+        estructuraCoches.put("coche" + (cont + (cont++ + 10)), new Coche("3456DFG", "BMW", "X5"));
+        estructuraCoches.put("coche" + (cont + (cont++ + 10)), new Coche("4567FGH", "AUDI", "A3"));
+
+        dbref.child("Coches").updateChildren(estructuraCoches);
+
+    }
+
+    private void probarSetValueObj() {
+        dbref.child("Coches").child("coche" + (cont++ + 10)).setValue(new Coche("1234BCD", "SEAT", "IBIZA"));
+    }
+
+
+    private void probarUpdateChildren() {
+        Map<String, Object> estructura = new HashMap<String, Object>();
+        estructura.put("animal1", "Perro");
+        estructura.put("animal2", "Gato");
+        estructura.put("animal3", "Raton");
+
+        dbref.child("Animales").updateChildren(estructura);
     }
 
     private void probarPush() {
---
+++ app/src/main/java/com/dam/pruebafirebaseauth/model/Coche.java
@@ -0,0 +1,36 @@
+package com.dam.pruebafirebaseauth.model;
+
+import java.io.Serializable;
+
+public class Coche implements Serializable {
+    private String matricula;
+    private String marca;
+    private String modelo;
+
+    public Coche(String matricula, String marca, String modelo) {
+        this.matricula = matricula;
+        this.marca = marca;
+        this.modelo = modelo;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    @Override
+    public String toString() {
+        return "Coche{" +
+                "matricula='" + matricula + '\'' +
+                ", marca='" + marca + '\'' +
+                ", modelo='" + modelo + '\'' +
+                '}';
+    }
+}
